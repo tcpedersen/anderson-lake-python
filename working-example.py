243d8f1b04f1db204bede2a2699c6d27a439b2ad
Tobias Cramer Pedersen <tobiascramerpedersen@gmail.com>
2020-03-26 09:43:03 +0100

    add GaussianQuadrature to working-example

--- working-example.py
+++ working-example.py
@@ -28,6 +28,21 @@ print(f"Price of option using anderson_lake_expsinh: {price}.")
 # Define the quadrature method the function should use to integrate. Here the 
 # one suggested in the Anderson-Lake article is used. It is implemented in 
 # integration.py
+scheme = ExpSinhQuadrature(0.5, 1e-12, 1000)
+
+# Define the model and option as in the simple case.
+model = HestonModel(100, 0.1197**2, 1.98937, 0.108977**2, 0.33147, 0.0258519, 0)
+option = EuropeanCallOption(1, 100)
+
+# Calculate the price (should return 4.171)
+price = anderson_lake(model, option, scheme)
+print(f"Price of option using anderson_lake: {price}.")
+
+# ==============================================================================
+# === Example using the less simple function anderson_lake
+# Define the quadrature method the function should use to integrate. Here the 
+# scipy implemented GaussianQuadrature is used. It is implemented in 
+# integration.py. This is the suggested method!
 scheme = GaussianQuadrature(1e-12, 1e-12, 1000)
 
 # Define the model and option as in the simple case.
